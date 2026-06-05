cd411a4f06c3c6333a26a272a1388cef6ec9c8e1
Yamamoto <2026012@fsc-educ.com>
2026-06-05 15:19:22 +0900

    ブラウザーでのページ表示確認

--- src/main/java/com/example/blog_app/BlogController.java
+++ src/main/java/com/example/blog_app/BlogController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BlogController {
     @GetMapping("/blog")
     public String blog() {
-        return "bolg";
+        return "blog";
     }
     
 }
